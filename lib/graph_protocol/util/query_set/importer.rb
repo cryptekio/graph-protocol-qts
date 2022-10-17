5a6b7285b9a931769222b6565e6210df72d22370
Nikola K <nikola@krzalic.com>
2022-10-17 11:58:33 +0200

    fix issues with splitting strings with non-ascii characters

--- lib/graph_protocol/util/query_set/importer.rb
+++ lib/graph_protocol/util/query_set/importer.rb
@@ -14,16 +14,14 @@ module GraphProtocol
 
         def self.import_qlog_chunk_from_s3(query_set:, range_start:, object_size:, chunk_size:)
           query_set_id = query_set.id
-          range_end = range_start + chunk_size
-
-          puts "Fetching buffer: start: #{range_start}, end: #{range_end} object_size: #{object_size}"
+          range_end = range_start + chunk_size-1
 
           args = { key: query_set.file_path,
                    range_start: range_start,
                    range_end: range_end }
           buffer = GraphProtocol::Util::S3::ObjectProcessor.get_object(args).body.read
 
-          rindex = buffer.rindex("\n")
+          rindex = buffer.bytes.rindex(10) # 10 == \n 
           next_range_start = rindex.nil? ? range_start : range_start+rindex+1
 
           schedule_import_job(query_set: query_set,
@@ -33,34 +31,38 @@ module GraphProtocol
 
           return if rindex.nil?
 
-          time = Time.now
-          lines = buffer[0..rindex-1].split("\n")
-          GraphProtocol::Util::Postgresql::Loader.execute!(query_set_id: query_set_id) do |copy|
-            lines.each_with_index do |line, index|
-              begin
-                copy << build_query_array(query_set_id: query_set_id,
-                                          time: time,
-                                          line: line)
-              rescue JSON::ParserError
-                puts "Index: #{index}"
-                puts "Line: #{line}"
-              end
-
-            end
-          end
+          lines = buffer.byteslice(0,rindex).split("\n")
+          write_queries(lines, query_set_id)
 
         end
 
         private
+
+          def self.write_queries(lines, query_set_id)
+            time = Time.now
+            GraphProtocol::Util::Postgresql::Loader.execute!(query_set_id: query_set_id) do |copy|
+              lines.each_with_index do |line, index|
+                copy << build_query_array(query_set_id: query_set_id,
+                                          time: time,
+                                          line: line)
+              end
+            end
+          end
+
           def self.build_query_array(line:, query_set_id:, time:)
             json_output = parsed_json(line)
             [json_output[:subgraph], { :query => json_output[:query] }.to_json, json_output[:variables], json_output[:timestamp], time, time, query_set_id]
           end
 
           def self.parsed_json(line)
-            parsed_data = JSON.parse(line.chomp, symbolize_names: true).except(:block, :time, :query_id)
-            parsed_data[:timestamp] = DateTime.parse(parsed_data[:timestamp]).to_f
-            parsed_data
+            begin
+              parsed_data = JSON.parse(line.chomp, symbolize_names: true).except(:block, :time, :query_id)
+              parsed_data[:timestamp] = DateTime.parse(parsed_data[:timestamp]).to_f
+              parsed_data
+            rescue JSON::ParserError
+              puts "Failed to parse query: #{line.chomp}"
+              {}
+            end
           end
 
           def self.set_status_importing(query_set)
