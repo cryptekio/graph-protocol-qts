b2f0d178ab1cfd43d619b1f6a7216a29e623a333
Nikola K <nikola@krzalic.com>
2023-02-11 13:08:19 +0100

    updated redis timeout values

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,4 @@
 Sidekiq.configure_server do |config|
   config.logger.level = Rails.logger.level
+  config.redis = { url: Rails.application.secrets.redis_url, network_timeout: 30, pool_timeout: 30, size: 1000 }
 end
